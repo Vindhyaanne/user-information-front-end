9fc137fe10f3a80bc0cdfadac8c37c8e9bfb70b9
Vindhyaanne <112675938+Vindhyaanne@users.noreply.github.com>
2022-09-01 21:25:55 -0700

    Add files via upload

--- src/components/ListUsersComponent.js
+++ src/components/ListUsersComponent.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import UserService from '../services/UserService';
-import axios from "axios";
-
 
 const ListUsersComponent = () => {
 
